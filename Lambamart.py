d24746ecc2ebdde97fa485b099e3fa240969a943
Tim Robben <t.a.a.robben@gmail.com>
2024-05-16 16:47:09 +0200

    gekut

--- Lambamart.py
+++ Lambamart.py
@@ -1,12 +1,11 @@
-import snowflake.connector
-import csv
 import lightgbm as lgb
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.utils import resample
+from relevance_score import relevance_score
 # Load the training dataset
 training_data = pd.read_csv("E:\VU\VU jaar 1\DMT\Ass_2\missing_imputed_country_id.csv")
-
+training_data = relevance_score(training_data)
 # Load the testing dataset
 testing_data = pd.read_csv("E:\VU\VU jaar 1\DMT\Ass_2\dmt-2024-2nd-assignment\\test_set_VU_DM.csv")
 # Feature engineering
@@ -25,6 +24,7 @@ features = ['site_id', 'visitor_location_country_id', 'prop_country_id', 'prop_i
             'search_month', 'search_day', 'search_hour', 'orig_destination_distance',
             'price_usd', 'srch_room_count', 'srch_saturday_night_bool']
 target = 'relevance_score'
+
 # Balance the dataset by upsampling the minority class
 df_majority = train_data[train_data[target] == 0]
 df_minority = train_data[train_data[target] > 0]
@@ -35,34 +35,24 @@ df_minority_upsampled = resample(df_minority,
                                  random_state=123) # reproducible results
 
 train_balanced = pd.concat([df_majority, df_minority_upsampled])
-
+qids_train = train_balanced.groupby("srch_id")["srch_id"].count().to_numpy()
+qids_test = test_data.groupby("srch_id")["srch_id"].count().to_numpy()
 # Split the balanced training data into training and validation sets
-X_train, X_val,Y_train, Y_val = train_test_split(train_balanced, test_size=0.2, random_state=42)
-
-# # Extract features (X) and labels (y) from the training data
-# X_train = training_data.drop(columns=['booking_bool'])  # Features
-# #y_click_train = training_data['click_bool']  # Click labels
-# y_booking_train = training_data['booking_bool']  # Booking labels
-
-# # For testing, we'll use the features from the testing dataset
-# X_test = testing_data
-qids_train = training_data.groupby("srch_id")["srch_id"].count().to_numpy()
-# # Split the training dataset into training and validation sets for click prediction
-# #X_click_train, X_click_val, y_click_train, y_click_val = train_test_split(X_train, y_click_train, test_size=0.2, random_state=42)
-
-# # Split the training dataset into training and validation sets for booking prediction
-# X_booking_train, X_booking_val, y_booking_train, y_booking_val = train_test_split(X_train, y_booking_train, test_size=0.2, random_state=42)
-qids_validation = y_booking_val.groupby("srch_id")["srch_id"].count().to_numpy()
-
-
-model = lgb.LGBMRanker(
-    objective="lambdarank",
-    metric="ndcg",
-)
-model.fit(
-    X=X_train,
-    y=Y_train,
-    eval_set=[(X_val, Y_val)],
-    eval_at=10,
-    verbose=10,
-)
+train_set, val_set = train_test_split(train_balanced, test_size=0.2, random_state=42)
+print(train_set[features].shape())
+print(train_set[target].shape())
+print(val_set[features].shape())
+print(val_set[target].shape())
+# model = lgb.LGBMRanker(
+#     objective="lambdarank",
+#     metric="ndcg",
+#     verbose=10
+# )
+# model.fit(
+#     X=train_set[features],
+#     y=train_set[target],
+#     group=qids_train,
+#     eval_set=[(val_set[features], val_set[target])],
+#     eval_group=[qids_test],
+#     eval_at=10,
+# )
--- relevance_score.py
+++ relevance_score.py
@@ -1,16 +1,18 @@
 import pandas as pd
 
-# Create a new column with initial value 0
-df['relevance_score'] = 0
+def relevance_score(df):
+    # Create a new column with initial value 0
+    df['relevance_score'] = 0
 
-# Set relevance score to 5 where booking_bool is 1
-df.loc[df['booking_bool'] == 1, 'relevance_score'] = 5
+    # Set relevance score to 5 where booking_bool is 1
+    df.loc[df['booking_bool'] == 1, 'relevance_score'] = 5
 
-# Set relevance score to 1 where click_bool is 1 and booking_bool is 0
-df.loc[(df['click_bool'] == 1) & (df['booking_bool'] == 0), 'relevance_score'] = 1
+    # Set relevance score to 1 where click_bool is 1 and booking_bool is 0
+    df.loc[(df['click_bool'] == 1) & (df['booking_bool'] == 0), 'relevance_score'] = 1
 
-# Calculate the percentage of rows with relevance_score above zero
-percentage_positive_relevance = (df['relevance_score'] > 0).mean() * 100
+    # Calculate the percentage of rows with relevance_score above zero
+    #percentage_positive_relevance = (df['relevance_score'] > 0).mean() * 100
 
-# Display the percentage
-print("Percentage of rows with relevance_score above zero:", percentage_positive_relevance)
+    # Display the percentage
+    #print("Percentage of rows with relevance_score above zero:", percentage_positive_relevance)
+    return df
